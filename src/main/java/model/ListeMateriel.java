0e0341b500add134fd393af66e0d046786fd46ca
clair <clair@10.134.34.104>
2024-04-18 16:59:14 +0200

    ajout des méthodes addMateriel pour les trois types de materiel

--- src/main/java/model/ListeMateriel.java
+++ src/main/java/model/ListeMateriel.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class ListeMateriel {
 	
@@ -42,7 +43,67 @@ public class ListeMateriel {
 		return -1;
 	}
 	
-	public void addMateriel() {}
+	/**
+	   * Ajoute un Materiel à lesMateriels si son nomProduit n'apparaît pas déjà dedans, et qu'il s'agit d'un AutreMateriel. 
+	   * 
+	   * @param nomProduit 			Le nomProduit du Materiel à ajouter
+	   * @param idColis 			Le idColis du Materiel à ajouter
+	   * @param quantite 			La quantite du Materiel à ajouter
+	   * @param unite 				La unite du Materiel à ajouter
+	   * @param volume 				Le volume du Materiel à ajouter
+	   * @return 
+	   */
+	public void addMateriel(String nomProduit, int idColis, int quantite, String unite, float volume) {
+		if (findMateriel(nomProduit) == -1)
+		{
+			Materiel materiel = new AutreMateriel(nomProduit, idColis, quantite, unite, volume);
+			this.lesColis.add(colis);
+		}
+	}
+	
+	/**
+	   * Ajoute un Materiel à lesMateriels si son nomProduit n'apparaît pas déjà dedans, et qu'il s'agit d'un MaterielMedical. 
+	   * 
+	   * @param nomProduit 			Le nomProduit du Materiel à ajouter
+	   * @param idColis 			Le idColis du Materiel à ajouter
+	   * @param quantite 			La quantite du Materiel à ajouter
+	   * @param unite 				La unite du Materiel à ajouter
+	   * @param volume 				Le volume du Materiel à ajouter
+	   * @param dlu 				La dlu du Materiel à ajouter
+	   * @param numeroLot 			Le numeroLot du Materiel à ajouter
+	   * @return 
+	   */
+	public void addMateriel(String nomProduit, int idColis, int quantite, String unite, float volume, Date dlu, int numeroLot) {
+		if (findMateriel(nomProduit) == -1)
+		{
+			Materiel materiel = new MaterielMedical(nomProduit, idColis, quantite, unite, volume, dlu, numeroLot);
+			this.lesColis.add(colis);
+		}
+	}
+	
+	/**
+	   * Ajoute un Materiel à lesMateriels si son nomProduit n'apparaît pas déjà dedans, et qu'il s'agit d'un Medicament. 
+	   * 
+	   * @param nomProduit 				Le nomProduit du Materiel à ajouter
+	   * @param idColis 				Le idColis du Materiel à ajouter
+	   * @param quantite 				La quantite du Materiel à ajouter
+	   * @param unite 					La unite du Materiel à ajouter
+	   * @param volume 					Le volume du Materiel à ajouter
+	   * @param dlu 					La dlu du Materiel à ajouter
+	   * @param numeroLot 				Le numeroLot du Materiel à ajouter
+	   * @param formeDosage 			La formeDosage du Materiel à ajouter
+	   * @param classeTherapeutique 	La classeTherapeutique du Materiel à ajouter
+	   * @param dci 					Le dci du Materiel à ajouter
+	   * @return 
+	   */
+	public void addMateriel(String nomProduit, int idColis, int quantite, String unite, float volume, Date dlu, int numeroLot, String dci, String classeTherapeutique, String formeDosage) {
+		if (findMateriel(nomProduit) == -1)
+		{
+			Materiel materiel = new Medicament(nomProduit, idColis, quantite, unite, volume, dlu, formeDosage, classeTherapeutique, dci, numeroLot);
+			this.lesColis.add(colis);
+		}
+	}
+	
 	public void deleteMateriel() {}
 	public void updateMateriel() {}
 
